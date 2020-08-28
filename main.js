05f1521191cfbf21abf1afdffdb3b7f5b2ba5690
namil9698 <69798566+namil9698@users.noreply.github.com>
2020-08-28 23:37:40 +0900

    JS:Remove active from nav.

--- main.js
+++ main.js
@@ -71,7 +71,13 @@ const projects = document.querySelectorAll('.project');
     if(filter == null) {
         return;
     }
-    projectContainer.classList.add('anim-out')
+
+    //Remove seletion from the previdus item and select the new one
+    const active = document.querySelector('.category__btn.selected')
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+        target.classList.add('selected');
+    
 
 
     setTimeout(()=> {
