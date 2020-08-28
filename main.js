e2f8d95ec371af3a247b270dc852bdce37c06951
namil9698 <69798566+namil9698@users.noreply.github.com>
2020-08-28 17:53:59 +0900

    JS:Home contact-btn

--- main.js
+++ main.js
@@ -24,7 +24,20 @@ navbarMenu.addEventListener('click',(event) => {
         return;
     }
 
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 });
+
+
+//Handle click on "contact me" button on home
+
+const ContactcMeBtn = document.querySelector('.home__contact');
+ContactcMeBtn.addEventListener('click',(event) =>{
+    scrollIntoView('#contact')
+})
+
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+    
+}
